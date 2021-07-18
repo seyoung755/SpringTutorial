95f8fb3533a9d1563c65e22c7880a1b2f189d072
seyeong <seyoung7555@naver.com>
2021-07-18 17:11:23 +0900

    User 정보 조회 및 수정 기능 구현
    user 조회 시 모든 유저 조회와 페이지 활용
    수정 시 Dirty Checking을 이용한 수정 구현

--- src/main/java/com/tutorial/springboot/costutorial/DummyControllerTest.java
+++ src/main/java/com/tutorial/springboot/costutorial/DummyControllerTest.java
@@ -4,11 +4,14 @@ import com.tutorial.springboot.domain.user.Role;
 import com.tutorial.springboot.domain.user.User;
 import com.tutorial.springboot.domain.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
+import java.util.List;
 import java.util.function.Supplier;
 
 @RestController
@@ -16,6 +19,42 @@ public class DummyControllerTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    // email, password 변경
+    @Transactional
+    @PutMapping("/dummy/user/{id}")
+    public User updateUser(@PathVariable int id, @RequestBody User requestUser) {
+        String email = requestUser.getEmail();
+        String password = requestUser.getPassword();
+
+        User user = userRepository.findById(id).orElseThrow(()-> {
+            return new IllegalArgumentException("수정에 실패하였습니다");
+        });
+//        userRepository.save(user.builder().
+//                password(password).
+//                email(email).
+//                build());
+        user.setEmail(email);
+        user.setPassword(password);
+        // save함수는 id에 해당하는 데이터가 없거나 id를 전달받지 않으면 insert를 하고
+        // 있는 경우 update 쿼리를 날린다.
+        userRepository.save(user);
+        return null;
+    }
+
+    @GetMapping("/dummy/users")
+    public List<User> list() {
+        return userRepository.findAll();
+    }
+
+    // 한 페이지당 2건에 데이터를 리턴받아 볼 예정
+    @GetMapping("/dummy/user")
+    public List<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<User> pagingUser = userRepository.findAll(pageable);
+        List<User> users = pagingUser.getContent();
+
+        return users;
+    }
 
     @GetMapping("/dummy/user/{id}")
     public User detail(@PathVariable int id) {
@@ -25,7 +64,7 @@ public class DummyControllerTest {
         User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
             @Override
             public IllegalArgumentException get() {
-                return new IllegalArgumentException("해당 유저는 없습니다 : "+id);
+                return new IllegalArgumentException("해당 유저는 없습니다 : " + id);
             }
         });
         // MessageConverter가 자바 오브젝트를 응답하면 Jackson 라이브러리를 호출하여 JSON으로 변환
